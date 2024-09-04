d14c97c47e8cbb7f690dea90b014b45d653dc2c9
dlwlsgur0909 <dlwlsgur0909@naver.com>
2024-09-04 17:42:02 +0900

    feat: 토큰 재발급 API 추가

--- src/js/auth.js
+++ src/js/auth.js
@@ -5,31 +5,52 @@ export const useAuthStore = defineStore({
     id: 'auth',
     state: () => ({
         member: JSON.parse(localStorage.getItem('member')),
-        isLogin: false
+        isLogin: false,
     }),
     actions: {
         setMember(member) {
             this.member = member;
-            localStorage.setItem('member', JSON.stringify(member));
             if(member === null) {
-                this.isLogin = false;
+							this.isLogin = false;
             }else {
-                this.isLogin = true;
+							this.isLogin = true;
+							localStorage.setItem('member', JSON.stringify(member));
             }
         },
         login(data, onSuccess, onReject) {
             axios
-            .post("http://localhost:8080/auth/login", data)
-            .then(res => {
-              onSuccess(res.data);
-            })
-            .catch(e => {
-              onReject(e);
-            })
+							.post("http://localhost:8080/auth/login", data)
+							.then(res => {
+								onSuccess(res.data);
+							})
+							.catch(e => {
+								onReject(e);
+							})
         },
         logout() {
-            localStorage.removeItem('member');
-            this.setMember(null);
+					this.setMember(null);
+					localStorage.removeItem('member');
+        },
+        async reissue() {
+
+					const request = {
+							refreshToken: this.member.refreshToken
+					};
+
+					let result = false;
+					
+					await axios
+						.post("http://localhost:8080/auth/reissue", request)
+						.then(res => {
+							this.setMember(res.data);
+							result = true;
+						})
+						.catch(e => {
+							this.logout();
+							alert('로그인이 만료되었습니다. 다시 로그인 해주세요.');
+						});
+
+					return result;
         }
     }
 
