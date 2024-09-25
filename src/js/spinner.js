97fc0a5d0ef222a0f3416c2d31c6b41bae9bedfb
dlwlsgur0909 <dlwlsgur0909@naver.com>
2024-09-25 17:12:01 +0900

    feat: Global Spinner 추가

---
+++ src/js/spinner.js
@@ -0,0 +1,16 @@
+import { defineStore } from 'pinia';
+
+export const useSpinnerStore = defineStore({
+    id: 'spinner',
+    state: () => ({
+		isLoading: false,
+    }),
+    actions: {
+		startSpinner() {
+			this.isLoading = true;
+		},
+        stopSpinner() {
+            this.isLoading = false;
+		}
+    }
+})
