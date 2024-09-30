92f8619b73627bef2dfeb73ed3eee737d55f9c02
dlwlsgur0909 <dlwlsgur0909@naver.com>
2024-09-30 16:12:22 +0900

    feat: customAxios spinner 적용

--- src/js/axios.js
+++ src/js/axios.js
@@ -8,6 +8,7 @@ const customAxios = () => {
 
     instance.interceptors.request.use(
         (config) => {
+            globalStore.spinner.startSpinner();
             const accessToken = auth.member.accessToken;
             config.headers['Authorization'] = `Bearer ${accessToken}`;
             return config;
@@ -19,6 +20,7 @@ const customAxios = () => {
 
     instance.interceptors.response.use(
         (response) => {
+            globalStore.spinner.stopSpinner();
             return response;
         },
         async (error) => {
@@ -26,10 +28,11 @@ const customAxios = () => {
                 if(await auth.reissue()) {
                     return await instance(error.config);
                 }else {
+                    globalStore.spinner.stopSpinner();
                     return Promise.reject();
                 }
-
             }else {
+                globalStore.spinner.stopSpinner();
                 globalStore.alert.openAlert(error.response.data.errorMessage);
                 return Promise.reject();
             }
