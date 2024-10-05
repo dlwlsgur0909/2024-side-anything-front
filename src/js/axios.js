a01c2da94a8b21da992b4bf3eda25fb72cc091d8
LJH09 <dlwlsgur0909@naver.com>
2024-10-05 15:29:21 +0900

    feat: custom axios BASE_URL 적용

--- src/js/axios.js
+++ src/js/axios.js
@@ -5,10 +5,18 @@ import globalStore from '../stores/globalStore.js';
 const customAxios = () => {
     const instance = axios.create();
     const auth = useAuthStore();
+    const env = import.meta.env;
+
+    const BASE_URL = 
+        env.MODE === 'development' ? env.VITE_LOCAL_SERVER : '';
 
     instance.interceptors.request.use(
         (config) => {
+            const path = config.url;
+            config.url = BASE_URL + path;
+
             globalStore.spinner.startSpinner();
+
             const accessToken = auth.member.accessToken;
             config.headers['Authorization'] = `Bearer ${accessToken}`;
             return config;
