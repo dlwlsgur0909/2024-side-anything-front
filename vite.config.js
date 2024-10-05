6c16b6aca2e1ca71b830c6db92130a8a8f22fa94
LJH09 <dlwlsgur0909@naver.com>
2024-10-05 15:06:58 +0900

    feat: 환경변수 관련 .env 파일 생성 및 vite.config에 백엔드 경로 설정

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,42 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+export default ({mode}) => {
+
+  const env = loadEnv(mode, '', '');
+  // console.log(env);
+
+  console.log(env);
+
+  const backendConfig = {
+    target:
+      mode === 'development' ? env.VITE_LOCAL_SERVER: '',
+    changeOrigin: true,
+    secure: false,
   }
-})
+
+  console.log(backendConfig);
+
+  return defineConfig({
+    plugins: [
+      vue(),
+      vueDevTools(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    server: {
+      proxy: {
+        '/auth' : backendConfig,
+      }
+    }
+  })
+}
+  
+  
