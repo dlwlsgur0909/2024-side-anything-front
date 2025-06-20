2239da6abaa65306df1824fbe8bee2b6b5bd7f3d
dlwlsgur0909 <dlwlsgur0909@naver.com>
2025-06-20 15:48:31 +0900

    fix: pinia 초기화전 reissue 호출 시 발생하는 에러 수정

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import './assets/css/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { useAuthStore } from './stores/authStore'
+import { useAlertStore } from './stores/alertStore'
 
 import App from './App.vue'
 import router from './router'
@@ -14,9 +15,13 @@ app.use(createPinia())
 app.provide('customAxios', customAxios());
 
 const auth = useAuthStore();
+const alert = useAlertStore();
 
 if(!!localStorage.getItem("ACCESS")) {
-  await auth.reissue();
+  if(!await auth.reissue()) {
+    router.push('/login');
+    alert.openAlert('로그인이 만료되었습니다');
+  }
 }else {
   auth.setMember(null);
 }
