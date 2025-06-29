434b0bd70fe2a205cf58efba63c6c152d4571f16
LJH09 <dlwlsgur0909@naver.com>
2025-06-29 20:18:49 +0900

    feat: stomp.js 추가 (확인 필요)

---
+++ src/utils/stomp.js
@@ -0,0 +1,57 @@
+import { Client } from "@stomp/stompjs";
+import SockJS from "sockjs-client";
+
+let stompClient = null;
+
+export const connectStomp = (roomId, memberId, onMessageReceived) => {
+
+    stompClient = new Client({
+        webSocketFactory: () => new SockJS("http://localhost:8090/ws-chat"),
+        reconnectDelay: 5000,
+        heartbeatIncoming: 4000,
+        heartbeatOutgoing: 4000,
+        debug: (message) => console.log('[STOMP DEBUG]', msg),
+        onConnect: () => {
+            console.log('WebSocket connected');
+
+            stompClient.subscribe(`/sub/chat/${roomId}`, (message) => {
+                const payload = JSON.parse(message.body);
+                onMessageReceived(payload);
+            });
+        },
+        onStompError: (frame) => {
+            console.error('STOMP Error: ', frame.headers['message']);
+            console.error('Detail: ', frame.body);
+        },
+    });
+
+    stompClient.activate();
+};
+
+export const sendMessage = (roomId, memberId, message) => {
+
+    if(!stompClient || !stompClient.connected) {
+        console.warn('STOMP NOT CONNECTED');
+        return;
+    }
+
+    const payload = {
+        messageType: 'TALK', 
+        roomId: roomId, 
+        memberId: memberId, 
+        message: message
+    };
+
+    stompClient.publish({
+        destination: `/pub/chat/${roomId}`,
+        body: JSON.stringify(payload),
+    });
+};
+
+export const disconnectStomp = () => {
+    if(stompClient) {
+        stompClient.deactivate();
+        stompClient = null;
+        console.log('WebSocket 연결 해제');
+    }
+}
