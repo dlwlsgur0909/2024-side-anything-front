7b73247bc60ca7c207bd91151e07669e79c136c0
LJH09 <dlwlsgur0909@naver.com>
2025-07-11 23:48:20 +0900

    refactor: stomp.js 에러 처리 및 주석 추가

--- src/utils/stomp.js
+++ src/utils/stomp.js
@@ -3,8 +3,9 @@ import SockJS from "sockjs-client";
 import { useAuthStore } from "@/stores/authStore";
 import globalStore from "@/stores/globalStore";
 
-let stompClient = null;
 const auth = useAuthStore();
+let stompClient = null;
+let retry = true;
 
 export const connectStomp = async (roomId, memberId, onMessageReceived) => {
 
@@ -14,12 +15,20 @@ export const connectStomp = async (roomId, memberId, onMessageReceived) => {
             Authorization: `Bearer ${auth.member.accessToken}` // CONNECT 프레임에 토큰 포함
         },
         reconnectDelay: 0, // 자동 재연결 비활성화 
-        heartbeatIncoming: 4000,
-        heartbeatOutgoing: 4000,
+        // heartbeatIncoming: 4000, // 서버에서 하트비트를 받는 주기 (좀 더 찾아봐야함)
+        // heartbeatOutgoing: 4000, // 서버로 하트비트를 보내는 주기 (좀 더 찾아봐야함)
         // debug: (message) => console.log('[STOMP DEBUG]', message), // 디버깅용
 
         // 연결 성공 시 자동으로 실행되는 콜백
         onConnect: () => {
+
+            // 클라이언트별 에러 큐 구독
+            stompClient.subscribe(`/user/${auth.member.id}/queue/errors`, (frame) => {
+                const response = JSON.parse(frame.body);
+                globalStore.alert.openAlert(response.errorMessage);
+                globalStore.router.push('/chatRoomList');
+            });
+
             // 입장한 채팅방을 구독
             stompClient.subscribe(`/sub/chat/${roomId}`, (message) => {
                 const payload = JSON.parse(message.body);
@@ -30,19 +39,21 @@ export const connectStomp = async (roomId, memberId, onMessageReceived) => {
         onStompError: async (frame) => {
 
             const errorCode = frame.headers['message'];
-
+            
             // 토큰 인증 실패인 경우 reissue 시도
-            if(errorCode === 'UNAUTHORIZED') {
-
+            if(errorCode === 'UNAUTHORIZED' && retry) {
                 if(await auth.reissue()) {
+                    retry = false;
                     stompClient.deactivate(); // 기존 연결 해제
                     await connectStomp(roomId, memberId, onMessageReceived); // 연결 재시도
+                    globalStore.alert.openAlert('잠시 후 다시 시도해주세요'); // 
                 }
 
             }else {
                 // 인증 오류가 아니라면 채팅방 목록으로 페이지 이동
                 globalStore.alert.openAlert('서버 오류입니다 다시 시도해주세요.');
                 globalStore.router.push('/chatRoomList');
+                retry = true;
             }
 
         },
@@ -52,10 +63,13 @@ export const connectStomp = async (roomId, memberId, onMessageReceived) => {
     stompClient.activate();
 };
 
-export const sendMessage = (roomId, memberId, message) => {
+// 메세지 전송
+export const sendMessage = async (roomId, memberId, message) => {
 
+    // 연결 상태 확인
     if(!stompClient || !stompClient.connected) {
-        console.warn('STOMP NOT CONNECTED');
+        globalStore.alert.openAlert('서버와 접속이 끊어졌습니다');
+        globalStore.router.push('/chatRoomList');
         return;
     }
 
@@ -73,12 +87,12 @@ export const sendMessage = (roomId, memberId, message) => {
             Authorization: `Bearer ${auth.member.accessToken}`
         }
     });
+
 };
 
 export const disconnectStomp = () => {
     if(stompClient) {
         stompClient.deactivate();
         stompClient = null;
-        console.log('WebSocket 연결 해제');
     }
-}
+};
